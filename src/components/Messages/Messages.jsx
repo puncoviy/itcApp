d9867c3992624b58bee494acd7d22704e7f02dd8
puncoviy <95302958+puncoviy@users.noreply.github.com>
2022-09-20 15:13:35 +0300

    feat: add messages basic styles

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import classes from './Messages.module.css'
 
-const Dialogs = (props) => {
+const Messages = (props) => {
     return (
-        <div>
-            Messages
+        <div className={classes.messages}>
+            <div className={classes.users}>
+                <ul className={classes.usersList}>
+                    <li className={classes.usersItem}>Viktor</li>
+                    <li className={classes.usersItem}>Pavel</li>
+                    <li className={classes.usersItem}>Petr</li>
+                    <li className={classes.usersItem}>Olga</li>
+                </ul>
+            </div>
+            <div className={classes.messagesBox}>
+                <ul className={classes.messageList}>
+                    <li className={classes.messageItem}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, aut!
+                    </li>
+                    <li className={classes.messageItem}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, aut!
+                    </li>
+                    <li className={classes.messageItem}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, aut!
+                    </li>
+                </ul>
+                <div className={classes.newMessage}>
+                    <textarea className={classes.input} placeholder='Enter your message'></textarea>
+                    <button className={classes.btn}>Send</button>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default Dialogs;
+export default Messages;
